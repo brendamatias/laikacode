16102d03424af1668c2810904b7d86d40e6e68e6
brendamatias <brendamatias.sobral@gmail.com>
2021-05-07 22:34:56 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: "My Gatsby Site",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "laikacode",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+  ],
+};
