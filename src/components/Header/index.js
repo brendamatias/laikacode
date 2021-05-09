df5c2541ccdac6fac6c4b3fbfab2d3922a8503fd
brendamatias <brendamatias.sobral@gmail.com>
2021-05-09 18:00:50 -0300

    fix: header fixed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,28 +2,41 @@ import * as React from 'react'
 import './styles.scss'
 
 import logoLaikaCode from '../../assets/images/logo.svg'
+import logoLaikaCodelLight from '../../assets/images/logo-light.svg'
 
-const links = ['Início', 'Sobre', 'Serviços', 'Projetos']
+const Header = () => {
+  const [offset, setOffset] = React.useState(0)
+  const links = ['Início', 'Sobre', 'Serviços', 'Projetos']
 
-const Header = () => (
-  <nav className="navbar">
-    <div className="container">
-      <img src={logoLaikaCode} alt="Logo LaikaCode" />
-      <ul>
-        {links.map((link) => (
+  React.useEffect(() => {
+    window.onscroll = () => {
+      setOffset(window.pageYOffset)
+    }
+  }, [])
+
+  return (
+    <nav className={`navbar ${offset > 10 ? 'navbar-fixed' : ''}`}>
+      <div className="container">
+        <img
+          src={offset > 10 ? logoLaikaCodelLight : logoLaikaCode}
+          alt="Logo LaikaCode"
+        />
+        <ul>
+          {links.map((link) => (
+            <li>
+              <a href="/#">{link}</a>
+            </li>
+          ))}
+
           <li>
-            <a href="/#">{link}</a>
+            <button className="btn-light" type="button">
+              Contato
+            </button>
           </li>
-        ))}
-
-        <li>
-          <button className="btn-light" type="button">
-            Contato
-          </button>
-        </li>
-      </ul>
-    </div>
-  </nav>
-)
+        </ul>
+      </div>
+    </nav>
+  )
+}
 
 export default Header
