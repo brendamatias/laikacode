722d1820131754ea18fcc6415af88fec8eaeea06
brendamatias <brendamatias.sobral@gmail.com>
2021-05-12 23:35:36 -0300

    fix: header active

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,11 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import * as React from 'react'
 import './styles.scss'
 
+import { Link } from 'react-scroll'
 import logoLaikaCode from '../../assets/images/logo.svg'
 
 const Header = () => {
-  // const path = window.location.href
   const [offset, setOffset] = React.useState(0)
   const links = [
     { id: 'home', label: 'Início' },
@@ -12,10 +13,6 @@ const Header = () => {
     { id: 'services', label: 'Serviços' },
     { id: 'projects', label: 'Projetos' },
   ]
-  // const { pathname, hash } = window.location
-  // React.useEffect(() => {
-  //   if (hash) window.location.href = `${pathname}${hash}`
-  // }, [hash])
 
   React.useEffect(() => {
     window.onscroll = () => {
@@ -27,13 +24,23 @@ const Header = () => {
     <nav className={`navbar ${offset > 10 ? 'navbar-fixed' : ''}`}>
       <div className="container">
         <img src={logoLaikaCode} alt="Logo LaikaCode" />
+
         <ul>
           {links.map((link) => (
             <li key={link.id}>
-              <a href={`/#${link.id}`}>{link.label}</a>
+              <Link
+                href="#"
+                activeClass="navbar-link__active"
+                to={`${link.id}`}
+                spy
+                smooth
+                offset={-101}
+                duration={400}
+              >
+                {link.label}
+              </Link>
             </li>
           ))}
-
           <li>
             <button className={offset > 10 ? '' : 'btn-light'} type="button">
               Traga seu projeto
